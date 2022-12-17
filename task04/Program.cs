f5977970788e25be2794dad5cd19cc7d5cb041eb
Evgeniy Volodin <volodin1981@yandex.ru>
2022-12-17 20:21:46 +0300

    all done

---
+++ task04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("введите первое число");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите первое второе");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int third = Convert.ToInt32(Console.ReadLine());
+int max = first;
+if (second > max)
+{
+    max = second;
+}
+if (third > max)
+{
+    max = third;
+}
+Console.WriteLine($" Максимальное число из введённых выше это {max}");
---
+++ task08/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите любое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (N<1)
+{
+    Console.WriteLine("Число меньше 1 тут не подходит, дружище. Только цыфры от 1 и больше");
+}
+else
+while (count <= N)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
