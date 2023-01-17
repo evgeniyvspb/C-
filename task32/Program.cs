b36db8081310055974851607b0baf838d3de547b
evgeniyvspb <volodin1981@yandex.ru>
2023-01-17 13:57:54 +0300

    done 31-33

---
+++ task32/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= rnd.Next(min,max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write(array[i]+",");
+        else Console.Write(array[i]);
+
+    }
+    Console.WriteLine("]");
+}
+
+void ChangeZnak(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= - array[i];
+    }
+}
+
+int[] create = CreateArrayRndInt(8,-100,100);
+PrintArray(create);
+ChangeZnak(create);
+PrintArray(create);
---
+++ task33/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= rnd.Next(min,max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write(array[i]+",");
+        else Console.Write(array[i]);
+
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNum(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true;
+    }
+    return false;
+}
+
+int[] arr = CreateArrayRndInt(8,0,9);
+PrintArray(arr);
+Console.WriteLine("Введите искомое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindNum(arr, num)?"yes":"no");
