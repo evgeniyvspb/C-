564338b09fa78e52b4ea2a674095c45a8aeede75
evgeniyvspb <volodin1981@yandex.ru>
2023-01-20 15:14:44 +0300

    until45

---
+++ task40/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Enter 3 numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1< num2 + num3 && num2 < num1 +num3 && num3 < num2+num3) Console.WriteLine("YES");
+else Console.WriteLine("NO");
---
+++ task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int ParseIn2(int num)
+{
+    if (num==0) return 0;
+    int result = num%2;
+    num /=2;
+    result += ParseIn2(num)*10;
+    return result; 
+}
+
+// Console.WriteLine(ParseIn2(Convert.ToInt32(Console.ReadLine())));
+Console.WriteLine("enter number");
+var num = Console.ReadLine();
+Console.WriteLine(ParseIn2(Convert.ToInt32(num)));
