988190a1558299dcfa9802bf168b8c449974236f
evgeniyvspb <volodin1981@yandex.ru>
2023-01-21 11:54:53 +0300

    added 34 y 36

---
+++ task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// /Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве.
+ 
+// [345, 897, 568, 234] -> 2
+ 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= rnd.Next(min,max+1);
+    }
+    return array;
+}
+ 
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write(array[i]+",");
+        else Console.Write(array[i]);
+ 
+    }
+    Console.Write("]");
+}
+ 
+int Chetnie (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2 == 0) count++;
+    }
+    return count;
+}
+ 
+int[] array = CreateArrayRndInt(10,100,999);
+int countChet = Chetnie(array);
+PrintArray(array);
+Console.Write($" -> {countChet}");
