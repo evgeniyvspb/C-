26a0826ac4a18cb4b34db19edc343417b147ace1
evgeniyvspb <volodin1981@yandex.ru>
2023-01-22 10:12:31 +0300

    all done until 45

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write(array[i]+",");
+        else Console.Write(array[i]);
+
+    }
+    Console.WriteLine("]");
+}
+
+int[] EnterUser()
+{
+    Console.WriteLine("Сколько чисел Вы собираетесь ввести?");
+    int count = Convert.ToInt32(Console.ReadLine());
+    int[] numbers = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"Введите число {i+1}:");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }    
+    return numbers;
+}
+
+int Counting(int[] number)
+{
+    int count = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i]>0) count++;
+    }
+    return count;
+}
+
+int[] num=EnterUser();
+int count = Counting(num);
+Console.WriteLine("Среди введенных чисел");
+PrintArray(num);
+Console.WriteLine($"больше нуля пользователь ввёл {count} раз");
---
+++ task43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями y = k1 *
+// x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// // пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 =
+// 9 -> (-0,5; -0,5)
+
+// x = (b2 - b1) / (k1 -k2)
+
+double[] EnterUser(int par)
+{
+    double[] numbers = new double[par];
+    for (int i = 0; i < par/2; i++)
+    {
+        Console.WriteLine($"Введите параметр k{i+1}:");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }    
+    for (int i = par/2; i < par; i++)
+    {
+        Console.WriteLine($"Введите параметр b{i-par/2+1}:");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return numbers;
+}
+
+(double,double) CountXY(double[] par)
+{
+    (double,double) tuple = (0, 0);
+    tuple.Item1 = ((par[3] - par[2])/(par[0]- par[1]));
+    tuple.Item2 = par[1]*tuple.Item1+par[3];
+    return tuple;
+}
+
+double[] arr =EnterUser(4);
+(double,double) tuple = CountXY(arr);
+Console.WriteLine($"точка пересечения двух прямых это ({tuple.Item1}; {tuple.Item2})");
