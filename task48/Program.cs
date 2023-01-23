af6e636e2051833a411f3e176f9ab819d1a484bd
evgeniyvspb <volodin1981@yandex.ru>
2023-01-23 15:15:00 +0300

    for homework

---
+++ task48/Program.cs
@@ -0,0 +1,33 @@
+﻿// Создать двухмерный массив Amn = m+m
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+int[,] array2D = CreateMatrix(3, 4, 1, 50);
+PrintMatrix(array2D);
