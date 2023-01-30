7161f7a8731f617211e70d4879d210986506ad8e
evgeniyvspb <volodin1981@yandex.ru>
2023-01-30 15:49:03 +0300

    \

--- task62/Program.cs
+++ task62/Program.cs
@@ -1,1 +1,46 @@
 ﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+void FillArraySpiral(int[,] squareMatrix)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+    while (temp <= squareMatrix.GetLength(0) * squareMatrix.GetLength(1))
+    {
+        squareMatrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < squareMatrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= squareMatrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > squareMatrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите количсетво строк в массиве");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количсетво столбцов в массиве");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row,column];
+//PrintArray(matrix);
+FillArraySpiral(matrix);
+PrintArray(matrix);
---
+++ task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8" 
+
+int mUser = InsertDigit("M:");
+int nUser = InsertDigit("N:");
+int sign = Math.Sign(mUser - nUser);
+IntegerNumber(nUser, mUser, sign);
+
+void IntegerNumber(int m, int n, int sign)
+{
+    if (m == n)
+    {
+        Console.WriteLine(n + "");
+        return;
+    }
+    IntegerNumber(n + sign, m, sign);
+    Console.WriteLine(n + "");
+}
+
+int InsertDigit(string text)
+{
+    int result; bool parse;
+    Console.WriteLine(text);
+    parse = Int32.TryParse(Console.ReadLine(), out result);
+    if (!parse) result = InsertDigit(text); //Если не корреткно ввёл заново вызывае метод
+    return result;
+}
